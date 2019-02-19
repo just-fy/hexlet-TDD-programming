35a30370fbc23270e274ac8c9cd82284c7e9ec82
Nastya Faizulina <just-fy@mail.ru>
2019-02-19 23:42:37 +0300

    make classes

---
+++ lesson8_classes/simpleCard.js
@@ -0,0 +1,11 @@
+// BEGIN (write your solution here)
+export default class SimpleCard {
+  constructor(name, damagePoints) {
+    this.name = name;
+    this.damagePoints = damagePoints;
+  }
+  damage(damagePoints) {
+    return this.damagePoints;
+  }
+}
+// END
